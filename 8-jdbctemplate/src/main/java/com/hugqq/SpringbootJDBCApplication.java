f146919841511542f72d5f89a16509bd7470114f
wq <330479321@qq.com>
2023-02-22 17:38:19 +0800

    :100:

---
+++ 8-jdbctemplate/src/main/java/com/hugqq/SpringbootJDBCApplication.java
@@ -0,0 +1,13 @@
+package com.hugqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJDBCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJDBCApplication.class, args);
+    }
+
+}
